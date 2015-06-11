3d2959b8e2e27398a6ebdb41c4e8b6952fd54cc4
mac <mac@macdeMacBook-Pro.local>
2015-06-11 15:20:04 +0800

    fix

--- package.js
+++ package.js
@@ -2,12 +2,16 @@ Package.describe({
   name: 'anderson916:meteor-alipay',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'a simple wrapper for alipay package',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/anderson916/meteor-alipay',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
+});
+
+Npm.depends({
+  'alipay-meteor-easy': '0.1.1'
 });
 
 Package.onUse(function(api) {
